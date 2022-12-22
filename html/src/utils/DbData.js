8e16f7d877fa3c7ea8b5f09c9bc82b2bad11d3f9
johnny <654499437@qq.com>
2022-12-22 21:50:16 +0800

    删除多余代码

--- html/src/utils/DbData.js
+++ html/src/utils/DbData.js
@@ -13,10 +13,38 @@ const DbData = {
     getDb() {
         return LocalData.data(datakey_db).db;
     },
-    userinfo: {
-        db: {}
-        , basePackageName: ''
-        , author: ''
+    cleanDb() {
+        LocalData.data(datakey_tables, null)
+    },
+    /** 保存用户信息 */
+    setUser(user) {
+        LocalData.data(datakey_user, {key: "user", value: user});
+    },
+    /** 获取用户信息 */
+    getUser() {
+        return LocalData.data(datakey_user).user;
+    },
+    /** 保存某一个数据库的表的集合 */
+    setTables(key, list) {
+        LocalData.data(datakey_tables, {key: key, value: list});
+    },
+    /** 获取这个库的表的集合 */
+    getTables(key) {
+        var obj = LocalData.data(datakey_tables);
+        return obj[key];
+    },
+    /** 获取表的详情 */
+    getTablesDetail(key, tableName) {
+        var list = getTables(key);
+        if (!list) {
+            return null;
+        }
+        for (let l of list) {
+            if (l.tableName == tableName) {
+                return l.columns;
+                break;
+            }
+        }
     }
 }
 
--- html/src/view/index.vue
+++ html/src/view/index.vue
@@ -45,35 +45,138 @@
 <script setup>
 import {ref, reactive, onMounted} from "vue";
 import DbData from '@/utils/DbData'
+import {message, Modal} from 'ant-design-vue';
+import Http from '@/utils/Http'
+import LocalData from "@/utils/localData";
+
+// 页面初始加载
+onMounted(() => {
+  reloadDbSelect()
+});
 
 const updateUrl = ref('/api/user/readDbCofig')
 const dbKey = ref('')
-
+const dbList = ref([{label: "选择数据-0", value: 0}])
+const tableList = ref([])
+const searchTableText = ref('')
+const userinfo = ref({db: {}})
 
 const handleFileChange = function (e) {
   if (e.file.status == 'done') {
     var res = e.fileList[0]
     if (res.status == 'done') {
       var data = res.response.data;
-      console.log(data)
-      DbData.setDb(data);
-      console.log(DbData.getDb())
-      // this.reloadDbSelect()
+      DbData.setDb(data)
+      reloadDbSelect()
     }
   }
 }
 // 从本地缓存中读取数据库下拉框
 const reloadDbSelect = function () {
-  let vm = this;
-  var dbList = getDb();
-  vm.dbList = [{label: "选择数据-0", value: 0}];
+  var arr = [{label: "选择数据", value: 0}];
   var count = 0;
-  for (let l in dbList) {
-    vm.dbList.push({label: l, value: l});
+  for (let l in DbData.getDb()) {
+    arr.push({label: l, value: l});
     count++;
   }
-  vm.dbList[0].label = '选择数据-' + count
+  // arr[0].label = '选择数据-' + count
+  dbList.value = arr
 }
+
+const selectDbValue = function () {
+  let val = dbKey.value
+  if (val != '0') {
+    var db = DbData.getDb()[val];
+    db.key = val;
+
+    userinfo.value.db = db;
+    DbData.setUser(userinfo.value);
+
+    reLoadTables();
+    // searchTableText.value = '';
+  } else {
+    tableList.value = [];
+  }
+}
+// 搜索输入框的回车键事件
+const reLoadTables = function () {
+  var key = userinfo.value.db.key;
+  if (key == 0 || !key) {
+    message.warning('需要选择一个数据源');
+    return;
+  }
+  var list = DbData.getTables(key);
+  tableList.value = list;
+
+  if (list) {
+    filterList(list);
+  } else {
+    Http.post('/api/db/getTables', userinfo.value.db)
+        .then(function (res) {
+          var list = res.data.data;
+          DbData.setTables(key, list);
+          filterList(list);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+  }
+  const filterList = function (list) {
+    if (searchTableText.value) {
+      var contentArr = searchTableText.value.split(",");
+      var arr = [];
+      for (let l of list) {
+        for (let content of contentArr) {
+          let includesFlag = false;
+          if (l.tableName.includes(content)) {
+            l.tableNameStr = l.tableName.replace(new RegExp(content, 'g'), `<b class="searchText">${content}</b>`);
+            includesFlag = true;
+          }
+          if (l.comment) {
+            if (l.comment.includes(content)) {
+              l.commentStr = l.comment.replace(new RegExp(content, 'g'), `<b class="searchText">${content}</b>`);
+              includesFlag = true;
+            }
+          }
+          if (includesFlag) {
+            arr.push(l)
+          }
+        }
+      }
+      list = arr;
+    }
+    if (list) {
+      for (var i = 0; i < list.length; i++) {
+        list[i].index = i + 1;
+        if (!list[i].tableNameStr) {
+          list[i].tableNameStr = list[i].tableName;
+        }
+        if (!list[i].commentStr) {
+          list[i].commentStr = list[i].comment;
+        }
+        list[i].index = i + 1;
+      }
+    }
+    tableList.value = list;
+    console.log(tableList.value)
+  }
+}
+
+// 清除数据库缓存
+const cleanDbCache = function () {
+  if (dbKey.value == '0') {
+    return;
+  }
+  Modal.confirm({
+    title: '确定要清除 ' + dbKey.value + ' 的所有缓存?',
+    onOk() {
+      DbData.cleanDb()
+      setTimeout(() => location.reload(), 1000);
+    },
+    class: 'test',
+  });
+}
+
 
 </script>
 
