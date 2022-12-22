4f75b2f18e7b9be4ab3531a722f433e9f3b68ce8
johnny <654499437@qq.com>
2022-12-22 19:31:08 +0800

    重新构建前端代码

--- html/src/main.js
+++ html/src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+//
+// import './assets/main.css'
+//
+// createApp(App).mount('#app')
 
-import './assets/main.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import App from './App';
+import 'ant-design-vue/dist/antd.css';
+
+const app = createApp(App);
+
+app.use(Antd).mount('#app');
