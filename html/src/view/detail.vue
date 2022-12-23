c82a192fd8f740b76f1469dc544a40a2195360ba
johnny <654499437@qq.com>
2022-12-23 17:05:20 +0800

    详情界面拆分

---
+++ html/src/view/detail.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <a-tabs v-model:activeKey="detailActiveKey">
+      <a-tab-pane key="field" tab="字段">
+        <!--          <h3>{{ detailDataSelectList.length }}</h3>-->
+        <a-table
+            :data-source="props.detailData.columns"
+            :columns="detailDataColumns"
+            row-key="id"
+            :pagination="false" size="small"
+            :row-selection="detailDataRowSelect"
+        >
+          <template #bodyCell="{ column, text, record }">
+            <template v-if="column.dataIndex === 'javaName'">
+              <span v-if="record.javaName!=record.name">{{ record.javaName }}</span>
+              <span v-else></span>
+            </template>
+          </template>
+        </a-table>
+      </a-tab-pane>
+      <a-tab-pane key="query" tab="查询sql">
+        <a-space>
+          <a-input v-model:value="as" style="width: 100px;" placeholder="表的别名"/>
+          代码样式
+          <a-switch v-model:checked="detailCodeShowSql" checked-children="开" un-checked-children="关"/>
+        </a-space>
+        <!-- div1 -->
+        <a-typography-paragraph :code="detailCodeShowSql" copyable class="liInfo_div">
+          select <br/>
+          <span v-for="(item, index) in detailDataSelectList">
+              <span>{{ ass }}{{ item.name }}
+                <span v-if="index<detailDataSelectList.length-1"> , </span>
+              </span>
+            </span><br/>
+          from {{ detailData.tableName }} {{ as }}
+        </a-typography-paragraph>
+        <!-- div2 -->
+        <a-typography-paragraph :code="detailCodeShowSql" copyable class="liInfo_div">
+          select <br/>
+          <span v-for="(item, index) in detailDataSelectList">
+              <span>
+                <span>{{ ass }}{{ item.name }}</span>
+                <span v-if="item.name != item.javaName"> '{{ item.javaName }}'</span>
+                <span v-if="index<detailDataSelectList.length-1"> , </span>
+              </span>
+            </span><br/>
+          from {{ detailData.tableName }} {{ as }}
+        </a-typography-paragraph>
+        <!-- div3 -->
+        <a-typography-paragraph :code="detailCodeShowSql" copyable class="liInfo_div">
+          select <br/>
+          <span v-for="(item, index) in detailDataSelectList">
+              <span>
+                <span>{{ ass }}{{ item.name }} '{{ item.comment || item.name }}'</span>
+                <span v-if="index<detailDataSelectList.length-1"> , </span>
+              </span>
+            </span><br/>
+          from {{ detailData.tableName }} {{ as }}
+        </a-typography-paragraph>
+      </a-tab-pane>
+      <a-tab-pane key="doc" tab="文档">
+        <a-space>
+          <a-radio-group v-model:value="liInfo_doc_class" button-style="solid">
+            <a-radio-button value="1">普通</a-radio-button>
+            <a-radio-button value="2">java1</a-radio-button>
+            <a-radio-button value="3">java2</a-radio-button>
+            <a-radio-button value="4">markdown</a-radio-button>
+          </a-radio-group>
+          代码样式
+          <a-switch v-model:checked="detailCodeShowDoc" checked-children="开" un-checked-children="关"/>
+        </a-space>
+        <a-typography-paragraph :code="detailCodeShowDoc" copyable class="liInfo_div">
+            <span v-if="liInfo_doc_class==4">
+                <span>字段  |  类型  | 描述</span> <br/>
+                <span>:----|:----|:----</span><br/>
+            </span>
+          <span v-for="item in detailDataSelectList">
+              <span v-if="liInfo_doc_class==1">
+                {{ item.javaName }} : {{ item.comment }}
+              </span>
+              <span v-if="liInfo_doc_class==2">
+                private {{ item.javaType }} {{ item.javaName }}; // {{item.comment}}
+              </span>
+              <span v-if="liInfo_doc_class==3">
+                /** {{item.comment}} */ <br/>
+                private {{ item.javaType }} {{ item.javaName }};
+              </span>
+              <span v-if="liInfo_doc_class==4">
+                  <span>{{ item.javaName }} | {{ item.javaType }} | {{item.comment}}</span>
+              </span>
+
+              <br/>
+            </span>
+        </a-typography-paragraph>
+      </a-tab-pane>
+      <a-tab-pane key="ddl" tab="DDL">
+        <a-space>
+          代码样式
+          <a-switch v-model:checked="detailCodeShowDDL" checked-children="开" un-checked-children="关"/>
+        </a-space>
+        <a-typography-paragraph :code="detailCodeShowDDL" copyable class="liInfo_div">
+          <a-typography-text v-html="detailData.ddl"></a-typography-text>
+          <br/>
+        </a-typography-paragraph>
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script setup>
+import {ref, reactive, computed, onMounted} from "vue";
+
+const props = defineProps(['detailData'])
+
+
+const detailActiveKey = ref('field')
+const detailDataColumns = ref([
+  {title: '字段', dataIndex: 'name', width: '200px'},
+  {title: '类型', dataIndex: 'type', width: '200px'},
+  {title: '注释', dataIndex: 'comment', ellipsis: true, width: '200px'},
+  {title: '驼峰', dataIndex: 'javaName'},
+])
+// 选中的表格数据
+const detailDataSelectList = ref([])
+const detailDataRowSelect = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    // console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+    detailDataSelectList.value = selectedRows
+  }
+};
+
+const detailCodeShowSql = ref(true)
+const detailCodeShowDoc = ref(true)
+const detailCodeShowDDL = ref(true)
+const as = ref('')
+const ass = computed(() => {
+  return as.value.length > 0 ? as.value + '.' : ''
+})
+
+// 文档上单选按钮
+const liInfo_doc_class = ref('1')
+
+
+</script>
+
+<style>
+.liInfo_div {
+  border: solid 1px #CCC;
+  padding: 10px;
+  margin-top: 10px;
+  height: auto;
+}
+</style>
--- html/src/view/index.vue
+++ html/src/view/index.vue
@@ -97,110 +97,7 @@
 
     <!--    模块框 -->
     <a-modal v-model:visible="detailLayerVisible" width="800px" :title="detailLayerTitle">
-      <a-tabs v-model:activeKey="detailActiveKey">
-        <a-tab-pane key="field" tab="字段">
-          <!--          <h3>{{ detailDataSelectList.length }}</h3>-->
-          <a-table
-              :data-source="detailData.columns"
-              :columns="detailDataColumns"
-              row-key="id"
-              :pagination="false" size="small"
-              :row-selection="detailDataRowSelect"
-          >
-            <template #bodyCell="{ column, text, record }">
-              <template v-if="column.dataIndex === 'javaName'">
-                <span v-if="record.javaName!=record.name">{{ record.javaName }}</span>
-                <span v-else></span>
-              </template>
-            </template>
-          </a-table>
-        </a-tab-pane>
-        <a-tab-pane key="query" tab="查询sql">
-          <a-space>
-            <a-input v-model:value="as" style="width: 100px;" placeholder="表的别名"/>
-            代码样式
-            <a-switch v-model:checked="detailCodeShowSql" checked-children="开" un-checked-children="关"/>
-          </a-space>
-          <!-- div1 -->
-          <a-typography-paragraph :code="detailCodeShowSql" copyable class="liInfo_div">
-            select <br/>
-            <span v-for="(item, index) in detailDataSelectList">
-              <span>{{ ass }}{{ item.name }}
-                <span v-if="index<detailDataSelectList.length-1"> , </span>
-              </span>
-            </span><br/>
-            from {{ detailData.tableName }} {{ as }}
-          </a-typography-paragraph>
-          <!-- div2 -->
-          <a-typography-paragraph :code="detailCodeShowSql" copyable class="liInfo_div">
-            select <br/>
-            <span v-for="(item, index) in detailDataSelectList">
-              <span>
-                <span>{{ ass }}{{ item.name }}</span>
-                <span v-if="item.name != item.javaName"> '{{ item.javaName }}'</span>
-                <span v-if="index<detailDataSelectList.length-1"> , </span>
-              </span>
-            </span><br/>
-            from {{ detailData.tableName }} {{ as }}
-          </a-typography-paragraph>
-          <!-- div3 -->
-          <a-typography-paragraph :code="detailCodeShowSql" copyable class="liInfo_div">
-            select <br/>
-            <span v-for="(item, index) in detailDataSelectList">
-              <span>
-                <span>{{ ass }}{{ item.name }} '{{ item.comment || item.name }}'</span>
-                <span v-if="index<detailDataSelectList.length-1"> , </span>
-              </span>
-            </span><br/>
-            from {{ detailData.tableName }} {{ as }}
-          </a-typography-paragraph>
-        </a-tab-pane>
-        <a-tab-pane key="doc" tab="文档">
-          <a-space>
-            <a-radio-group v-model:value="liInfo_doc_class" button-style="solid">
-              <a-radio-button value="1">普通</a-radio-button>
-              <a-radio-button value="2">java1</a-radio-button>
-              <a-radio-button value="3">java2</a-radio-button>
-              <a-radio-button value="4">markdown</a-radio-button>
-            </a-radio-group>
-            代码样式
-            <a-switch v-model:checked="detailCodeShowDoc" checked-children="开" un-checked-children="关"/>
-          </a-space>
-          <a-typography-paragraph :code="detailCodeShowDoc" copyable class="liInfo_div">
-            <span v-if="liInfo_doc_class==4">
-                <span>字段  |  类型  | 描述</span> <br/>
-                <span>:----|:----|:----</span><br/>
-            </span>
-            <span v-for="item in detailDataSelectList">
-              <span v-if="liInfo_doc_class==1">
-                {{ item.javaName }} : {{ item.comment }}
-              </span>
-              <span v-if="liInfo_doc_class==2">
-                private {{ item.javaType }} {{ item.javaName }}; // {{item.comment}}
-              </span>
-              <span v-if="liInfo_doc_class==3">
-                /** {{item.comment}} */ <br/>
-                private {{ item.javaType }} {{ item.javaName }};
-              </span>
-              <span v-if="liInfo_doc_class==4">
-                  <span>{{ item.javaName }} | {{ item.javaType }} | {{item.comment}}</span>
-              </span>
-
-              <br/>
-            </span>
-          </a-typography-paragraph>
-        </a-tab-pane>
-        <a-tab-pane key="ddl" tab="DDL">
-          <a-space>
-            代码样式
-            <a-switch v-model:checked="detailCodeShowDDL" checked-children="开" un-checked-children="关"/>
-          </a-space>
-          <a-typography-paragraph :code="detailCodeShowDDL" copyable class="liInfo_div">
-            <a-typography-text v-html="detailData.ddl"></a-typography-text>
-            <br/>
-          </a-typography-paragraph>
-        </a-tab-pane>
-      </a-tabs>
+      <DetailPage :detailData="detailData"/>
     </a-modal>
   </div>
 </template>
@@ -211,6 +108,8 @@ import DbData from '@/utils/DbData'
 import {message, Modal} from 'ant-design-vue';
 import Http from '@/utils/Http'
 import LocalData from "@/utils/localData";
+
+import DetailPage from '@/view/detail.vue'
 
 // 页面初始加载
 onMounted(() => {
@@ -359,50 +258,8 @@ const cleanCache = function (row) {
   DbData.setTablesDetail(userinfo.value.db.key, row.tableName, null);
 }
 
-// 列表加载
-function initcolumns(queryTable) {
-  detailLayerVisible.value = true
-  detailActiveKey.value = 'field'
-  detailLayerTitle.value = queryTable.comment + " : " + queryTable.tableName
-
-  detailData.value = queryTable;
-}
-
-// ------------- 弹出层
-// 详情弹出层
-const detailLayerVisible = ref(false)
-const detailLayerTitle = ref('xxx:xxx')
-const detailActiveKey = ref('field')
-const layerTitle = ref('')
-const detailData = ref({columns: [], ddl: ''})  // 选中的表的数据
-const detailDataColumns = ref([
-  {title: '字段', dataIndex: 'name', width: '200px'},
-  {title: '类型', dataIndex: 'type', width: '200px'},
-  {title: '注释', dataIndex: 'comment', ellipsis: true, width: '200px'},
-  {title: '驼峰', dataIndex: 'javaName'},
-])
-// 选中的表格数据
-const detailDataSelectList = ref([])
-const detailDataRowSelect = {
-  onChange: (selectedRowKeys, selectedRows) => {
-    // console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-    detailDataSelectList.value = selectedRows
-  }
-};
-
-const detailCodeShowSql = ref(true)
-const detailCodeShowDoc = ref(true)
-const detailCodeShowDDL = ref(true)
-const as = ref('')
-const ass = computed(() => {
-  return as.value.length > 0 ? as.value + '.' : ''
-})
-
-const liInfo_doc_class = ref('1')
-
 /*界面详情按钮*/
 function detailLayerClick(row) {
-  layerTitle.value = `${row.comment} : ${row.tableName}`
   var queryTable = DbData.getTablesDetail(dbKey.value, row.tableName);
   if (!queryTable) {
     var queryData = userinfo.value.db;
@@ -422,6 +279,18 @@ function detailLayerClick(row) {
     initcolumns(queryTable);
     row.columns = queryTable.columns;
   }
+}
+
+// ------------- 弹出层
+const detailLayerVisible = ref(false)
+const detailLayerTitle = ref('xxx:xxx')
+const detailData = ref({columns: [], ddl: ''})  // 选中的表的数据
+
+// 列表加载
+function initcolumns(queryTable) {
+  detailLayerVisible.value = true
+  detailLayerTitle.value = queryTable.comment + " : " + queryTable.tableName
+  detailData.value = queryTable;
 }
 
 </script>
@@ -455,10 +324,4 @@ function detailLayerClick(row) {
   cursor: pointer;
 }
 
-.liInfo_div {
-  border: solid 1px #CCC;
-  padding: 10px;
-  margin-top: 10px;
-  height: auto;
-}
 </style>
