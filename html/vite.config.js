0fa86c3260413689beacfa4e8df11434274ce86d
johnny <654499437@qq.com>
2022-12-24 14:32:59 +0800

    修改配置

--- html/vite.config.js
+++ html/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         }
     },
     build: {
-        // outDir: '../src/main/resources/public',
+        outDir: '../src/main/resources/public',
         // outDir: '../target/classes/public',
         emptyOutDir: true
     },
