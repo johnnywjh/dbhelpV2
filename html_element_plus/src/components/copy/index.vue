d8d4fcbe1b564c86fb44017904146b40e2362f2c
johnny <654499437@qq.com>
2023-02-18 19:58:05 +0800

    修改复制核心方法

--- html_element_plus/src/components/copy/index.vue
+++ html_element_plus/src/components/copy/index.vue
@@ -28,14 +28,44 @@ const props = defineProps({
 
 const showCopyIcon = ref(true)
 const copyClick = () => {
-  if (!isSupported) {
-    ElMessage.error("您的浏览器不支持复制功能 (Clipboard API)");
+  if(!props.content){
+    ElMessage.error("复制内容为空");
     return;
   }
-  copy(props.content)
-  // ElMessage.success('copy success')
-  showCopyIcon.value = false
-  setTimeout(() => showCopyIcon.value = true, 3000);
+
+  // if (!isSupported) {
+  //   ElMessage.error("您的浏览器不支持复制功能 (Clipboard API)");
+  //   return;
+  // }
+
+  // copy(props.content)   // 这个在linux docker 上无效,不知道是什么原因
+  // showCopyIcon.value = false
+  // setTimeout(() => showCopyIcon.value = true, 3000);
+
+  copyText(props.content)
+}
+
+function copyText(val){
+  // 动态创建 textarea 标签
+  const textarea = document.createElement('textarea');
+  // 将该 textarea 设为 readonly 防止 iOS 下自动唤起键盘，同时将 textarea 移出可视区域
+  textarea.readOnly = 'readonly';
+  textarea.style.position = 'absolute';
+  textarea.style.left = '-9999px';
+  // 将要 copy 的值赋给 textarea 标签的 value 属性
+  textarea.value = val;
+  // 将 textarea 插入到 body 中
+  document.body.appendChild(textarea);
+  // 选中值并复制
+  textarea.select();
+  textarea.setSelectionRange(0, textarea.value.length);
+  const result = document.execCommand('Copy');
+  if (result) {
+    // console.log('复制成功');
+    showCopyIcon.value = false
+    setTimeout(() => showCopyIcon.value = true, 3000);
+  }
+  document.body.removeChild(textarea);
 }
 
 </script>
